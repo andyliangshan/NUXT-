628b3766668ef0e9266f83cbb779269143daeb2e
noodless <noodleswang@shunshunliuxue.com>
2018-04-09 15:31:13 +0800

    init

---
+++ components/DataList/DataList.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="index-datalist" id="wrapper">
+    <div class="datalist" id="myScrollbar">
+      <div class="tipsNews">5条新币文</div>
+      <div class="dataListCont">
+        <div class="list-top row">
+          <div class="list-top-profile col-2"><img src="../../assets/img/person-pic.png" alt="profile"/></div>
+          <div class="list-top-info col-8">
+            <div class="list-top-info-title"><router-link :to="'/masterState/'" class="backtoPage">MXinsa</router-link></div>
+            <div class="list-top-info-publishTime"><span>39分钟前</span><em><i></i>精选</em></div>
+          </div>
+          <div class="list-top-attent col-2">
+            <!--<a href="javascript:void(0)" class="delete" ref="deleteBtn">删除</a>-->
+            <a href="javascript:void(0)" class="attention" ref="attentBtn">关注</a>
+            <a href="javascript:void(0)" class="report">×</a>
+          </div>
+        </div>
+        <div class="list-mid">
+          <div class="list-mid-publish-content">
+            <div class="contDesc" ref="contDesc">大撒上大叔大叔大叔大叔阿斯顿发发多少</div>
+            <div class="queryDetail"><router-link :to="'/detail/'" class="backtoPage">&nbsp;</router-link></div>
+            <p>查看详情</p>
+          </div>
+          <div class="list-mid-publish-img">
+            <span><img src="../../assets/img/profile-ho.png" alt="profile-ho"/></span>
+          </div>
+        </div>
+        <div class="list-bot">
+          <div class="coin"><span>&nbsp;</span><em>9636</em></div>
+          <div class="dianzan col-2 active"><span>&nbsp;</span><em>9636</em></div>
+          <div class="sendmsg col-2"><span>&nbsp;</span>8363</div>
+          <div class="share col-7"><span>&nbsp;</span>3.2K</div>
+        </div>
+      </div>
+      <div class="dataListCont">
+        <div class="list-top row">
+          <div class="list-top-profile col-2"><img src="../../assets/img/person-pic.png" alt="profile"/></div>
+          <div class="list-top-info col-8">
+            <div class="list-top-info-title"><router-link :to="'/masterState/'" class="backtoPage">MXinsa</router-link></div>
+            <div class="list-top-info-publishTime"><span>39分钟前</span></div>
+          </div>
+          <div class="list-top-attent col-2">
+            <a href="javascript:void(0)" class="attention" ref="attentBtn">关注</a>
+            <a href="javascript:void(0)" class="report">×</a>
+          </div>
+        </div>
+        <div class="list-mid">
+          <div class="list-mid-publish-content">
+            <div class="contDesc" ref="contDesc">大撒上大叔大叔大叔大叔阿斯顿发发多少</div>
+            <div class="queryDetail"><router-link :to="'/detail/'" class="backtoPage">&nbsp;</router-link></div>
+            <p>查看详情</p>
+          </div>
+          <div class="list-mid-publish-img">
+            <span><img src="../../assets/img/profile-ho.png" alt="profile-ho"/></span>
+          </div>
+        </div>
+        <div class="list-bot">
+          <div class="coin"><span>&nbsp;</span><em>9636</em></div>
+          <div class="dianzan col-2 active"><span>&nbsp;</span><em>9636</em></div>
+          <div class="sendmsg col-2"><span>&nbsp;</span>8363</div>
+          <div class="share col-7"><span>&nbsp;</span>3.2K</div>
+        </div>
+      </div>
+      <div class="dataListCont">
+        <div class="list-top row">
+          <div class="list-top-profile col-2"><img src="../../assets/img/person-pic.png" alt="profile"/></div>
+          <div class="list-top-info col-8">
+            <div class="list-top-info-title"><router-link :to="'/masterState/'" class="backtoPage">MXinsa</router-link></div>
+            <div class="list-top-info-publishTime"><span>39分钟前</span></div>
+          </div>
+          <div class="list-top-attent col-2">
+            <a href="javascript:void(0)" class="attention" ref="attentBtn">关注</a>
+            <a href="javascript:void(0)" class="report">×</a>
+          </div>
+        </div>
+        <div class="list-mid">
+          <div class="list-mid-publish-content">
+            <div class="contDesc" ref="contDesc">大撒上大叔大叔大叔大叔阿斯顿发发多少</div>
+            <div class="queryDetail"><router-link :to="'/detail/'" class="backtoPage">&nbsp;</router-link></div>
+            <p>查看详情</p>
+          </div>
+          <div class="list-mid-publish-img">
+            <span><img src="../../assets/img/profile-ho.png" alt="profile-ho"/></span>
+          </div>
+        </div>
+        <div class="list-bot">
+          <div class="coin"><span>&nbsp;</span><em>9636</em></div>
+          <div class="dianzan col-2 active"><span>&nbsp;</span><em>9636</em></div>
+          <div class="sendmsg col-2"><span>&nbsp;</span>8363</div>
+          <div class="share col-7"><span>&nbsp;</span>3.2K</div>
+        </div>
+      </div>
+      <div class="pullUp"></div>
+    </div>
+    <!--<report-list v-show="reportListPop"></report-list>-->
+  </div>
+</template>
+
+<script>
+  // import ReportList from '../../components/ReportList.vue';
+  export default {
+    name: 'index-datalist',
+    data () {
+      return {
+        reportListPop: false
+      }
+    },
+    components: {
+      // ReportList,
+    }
+  }
+</script>
+<style lang="stylus">
+@import "./datalist.styl";
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starte12342134r',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project22222' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css'],
+  // Global env
+  env: {
+    __ENV: process.env.__ENV
+  },
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        // 拓展 webpack 配置
+        // config.entry['polyfill'] = ['babel-polyfill']
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        // 添加 alias 配置
+        Object.assign(config.resolve.alias, {
+          'utils': path.resolve(__dirname, 'utils'),
+          'assets': path.resolve(__dirname, 'assets')
+        })
+      }
+    }
+  }
+}
