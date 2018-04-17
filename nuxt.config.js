4aad0390f30f35fbbf118026778680df35d5c5fb
Arthur <arthur@kite-js.com>
2018-04-17 17:21:10 +0800

    引入vue移动设备 touch 事件相关包

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,36 +2,46 @@ const path = require('path')
 
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'start',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project22222' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project22222'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: ['~/assets/css/main.css'],
   // Global env
   env: {
     __ENV: process.env.__ENV
   },
   /*
-  ** Add axios globally
-  */
+   ** Add axios globally
+   */
   build: {
     vendor: ['axios'],
     /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
+     ** Run ESLINT on save
+     */
+    extend(config, ctx) {
       if (ctx.isClient) {
         // 拓展 webpack 配置
         // config.entry['polyfill'] = ['babel-polyfill']
@@ -50,4 +60,3 @@ module.exports = {
     }
   }
 }
-
--- store/index.js
+++ store/index.js
@@ -2,8 +2,10 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import createLogger from 'vuex/dist/logger'
+import Vue2TouchEvents from 'vue2-touch-events'
 
 Vue.use(Vuex);
+Vue.use(Vue2TouchEvents);
 
 export function createStore() {
     return new Vuex.Store({
